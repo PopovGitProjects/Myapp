4d0b20f4484e842bfb142bd90006851c59a3a31b
Sergey Popov <popov.workmail@gmail.com>
2021-11-19 17:02:15 +0500

    Новый проект.

--- app/src/main/java/com/popov/myapp/MainActivity.kt
+++ app/src/main/java/com/popov/myapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.popov.myapp.databinding.ActivityMainBinding
 
@@ -14,18 +15,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result: ActivityResult ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                binding.mainActivity.text = result.data?.getStringExtra("key")
-            }
-        }
-
-        binding.goToTest.setOnClickListener {
-            val intent = Intent(this, Test:: class.java)
-            intent.putExtra("key", "How are you?")
-            startForResult.launch(intent)
-        }
     }
 }
--- app/src/main/java/com/popov/myapp/SIgnInUpActivity.kt
+++ app/src/main/java/com/popov/myapp/SIgnInUpActivity.kt
@@ -1,25 +1,15 @@
 package com.popov.myapp
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.popov.myapp.databinding.ActivityTestBinding
 
-class Test : AppCompatActivity() {
+class SIgnInUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val message = "Intent message: " + intent.getStringExtra("key")
-
-        binding.textView2.text = message
-
-        binding.button4.setOnClickListener {
-            intent.putExtra("key", binding.editText.text)
-            setResult(RESULT_OK, intent)
-            finish()
-        }
     }
 }

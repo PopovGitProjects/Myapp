8f2d31a7ab956325bdeadf3a1f05da578b93579e
Sergey Popov <popov.workmail@gmail.com>
2021-11-29 17:31:55 +0500

    Переделал второе активити, дабавил колбэки и список переменных.

--- app/src/main/java/com/popov/myapp/MainActivity.kt
+++ app/src/main/java/com/popov/myapp/MainActivity.kt
@@ -1,19 +1,48 @@
 package com.popov.myapp
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.popov.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var login: String = ""
+    private var password: String = ""
+    private var firstName: String = ""
+    private var secondName: String = ""
+    private var imageID: Int = 0
+
+    private lateinit var launcherSignIn: ActivityResultLauncher<Intent?>
+    private lateinit var launcherSignUp: ActivityResultLauncher<Intent?>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        launcherSignIn = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result: ActivityResult ->
+            if (result.resultCode == RESULT_OK){
+                val callback = result.data
+            }
+        }
+        launcherSignUp = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result: ActivityResult ->
+            if (result.resultCode == RESULT_OK){
+                val callback = result.data
+            }
+        }
+
+        binding.signIn.setOnClickListener {
+
+        }
+        binding.signUpAndClose.setOnClickListener {
+
+        }
     }
 }
